7b7dfd2f1d56a8c00a157215786f6247467025b0
Rylie Johnson <ryliexjohnson@gmail.com>
2022-03-28 01:58:36 -0400

    stable i think but takes forever to run

--- multilayer-perceptron/start.py
+++ multilayer-perceptron/start.py
@@ -1,6 +1,13 @@
+from codecs import BufferedIncrementalEncoder
 import csv
 import math
 import numpy as np
+import random
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimage
+
+import PIL
 
 zeroToFour = False
 
@@ -50,10 +57,12 @@ def g(x):
     return 1 / (1 + math.e**(-x))
     #return 1 / (1 + np.power(math.e, -x))
 
+"""
 #may not use because order of operations or whatever
 #ONLY VALID WHEN g() IS SIGMOID!!
 def gPrime(x):
     return g(x) * (1 - g(x))
+"""
 
 def importData(filename):
     #data = np.array(0)
@@ -72,7 +81,7 @@ def importData(filename):
     return np.array(data)
 
 
-
+"""
 #takes all the activations times the weights to get that neuron's input value
 #working!
 def calculateIntoNeuron(activations, weights):
@@ -87,34 +96,44 @@ def newWeightUpdate(weight, alpha, prevActivation, gin, expected):
     #return weight + alpha * error * gPrime(gin) * myinput
     #hard coding gPrime in rather than using the function, for order of operations purposes
     return weight + alpha * error * gin * (1 - gin) * prevActivation #gPrime(gin) * myinput
+"""
 
-
-
+"""
 #where input is the activations times weights
 #delta
 def modifiedError(error, input):
     return error * g(input) * (1 - g(input))
+"""
 
+
+"""
 #where activation is the activation of the previous neuron 
 #input is the summed activations times weights of the following neuron
 #weight is the weight between these
 def modifiedWeightUpdate(weight, alpha, activation, error, input):
     return weight + alpha * activation * modifiedError(error, input)
+"""
 
+"""
 #where input is the summed activations times weights of the concerned neuron
 #weights is all the weights connecting the concerned neuron to the next layer
 #and same for deltas
 def myDelta(input, weights, deltas):
     return g(input) * (1 - g(input)) * sum(weights*deltas)
+"""
 
+"""
 #ERROR: weird matrix stuff possible - be careful!
 def internalWeightUpdate(weight, alpha, activation, delta):
     #return weight + alpha * activation * delta
     return weight + alpha * np.dot(activation, delta)
+"""
 
+"""
 def getInnerDelta(activation, weight, prevDelta):
     return activation * (1 - activation) * weight * prevDelta
     #return activation * (1 - activation) * np.dot(weight, prevDelta)
+"""
 
 
 
@@ -127,8 +146,7 @@ def getInnerDelta(activation, weight, prevDelta):
 
 
 
-
-
+"""
 #feed forward an input through the NN, 'making a guess' 
 def feedForward(point):
     #feed into hidden layer
@@ -144,7 +162,10 @@ def feedForward(point):
     finalOut = g(finalInputs)
 
     return hiddenInputs, hiddenOut, finalInputs, finalOut
+"""
 
+
+"""
 #guess being the output layer
 def getError(guess, point):
     #the index referred to by point should be the only index with a value of 0
@@ -165,7 +186,7 @@ def getError(guess, point):
 
     return totalError
 
-
+"""
 
 
 
@@ -187,17 +208,18 @@ else:
     outputWeights = np.random.uniform(-1, 1, size=(28, 2))
 
 data = importData("data/mnist_train_0_1.csv")
+data = importData("data/mnist_train_0_4.csv")
 initX = data[0]
 #TODO: bias???
 initX[0] = 1
 
 #finalInputs is the hidden activations times the corresponding weights. 
-hiddenInputs, hiddenNeurons, finalInputs, outputNeurons = feedForward(initX)
+#hiddenInputs, hiddenNeurons, finalInputs, outputNeurons = feedForward(initX)
 
 
 #print(outputNeurons)
 #print(outputWeights)
-err = getError(outputNeurons, data[0])
+#err = getError(outputNeurons, data[0])
 #print(err)
 #print()
 
@@ -231,7 +253,7 @@ print()"""
 
 
 
-
+"""
 def updateWeights(neurons, weights, inputs, alpha, err, point):
     updated = np.zeros((len(weights), len(neurons)))
     #if len(neurons) == 2:
@@ -262,8 +284,10 @@ def updateWeights(neurons, weights, inputs, alpha, err, point):
         #updated.append(newWeights)
     return updated
 #TODO: what about X0 = 1?
+"""
 
 
+"""
 
 hiddenInputs, hiddenNeurons, finalInputs, outputNeurons = feedForward(initX)
 err = getError(outputNeurons, data[0])
@@ -282,105 +306,49 @@ def fullUpdate(outputNeurons, outputWeights, finalInputs, err, hiddenNeurons, hi
 
 
 
-
-
-"""
-#sanity check testing:
-#x1=3, x2=2, x3=4
-rawInput = np.array([3, 2, 4])
-#w1,y=0.7 w2,y=1.5 w3,y=-0.2
-initWeights = np.array([0.7, 1.5, -0.2])
-alpha = 0.5
-expectedValue = 0
-
-#calculate in
-testingInput = calculateIntoNeuron(rawInput, initWeights)
-print(testingInput)
-
-gin = g(testingInput)
-print(gin)
-
-#pass in the weight and its corresponding input (the activation from the previous layer)
-updatedTestWeights = []
-for i in range(len(initWeights)):
-    updatedTestWeights.append(newWeightUpdate(initWeights[i], alpha, rawInput[i], gin, expectedValue))
-    print(updatedTestWeights[i])
-"""
-
-"""
-
-xs = np.array([-3, 2, -1])
-#wh = np.array([[-2, 0.5, 0.75], [-1.5, -4, 0.6]])
-wh = np.array([[-2, -1.5], [0.5, -4], [0.75, 0.6]])
-hs = []
-wo = np.array([-0.25, 0.1])
-dh = []
-out = 0
-alpha = 0.5
-expected = 1
-
-
-xs = np.array([data[0]]).T
-wh = hiddenWeights
-wo = outputWeights
-expected = data[0][0]
-
-
-
-
-#h1 should be  0.998
-#h2 should be 0.016
-#out should be 0.438
-
-#do should be 0.138
-#dh1 should be 0.00007
-#dh2 should be 0.00021
-
-#next wo should be -0.181
-#next wh should be [-1.99, ?]
-
-hs = np.dot(wh.T, xs)
-hs = g(hs)
-print(hs)
-
-out = np.dot(wo.T, hs)
-out = g(out)
-print(out)
-
-err = expected - out
-
-do = modifiedError(err, out)
-print(do)
-
-dh = getInnerDelta(hs, wo, do)
-print(dh)
-
-#wo = modifiedWeightUpdate(wo, alpha, hs, err, )
-wo = internalWeightUpdate(wo, alpha, hs, do)
-print(wo)
-
-wh = internalWeightUpdate(wh, alpha, np.array([xs]).T, np.array([dh]))
-print(wh)
-        
 """
 
 
-xs = np.array([data[0][1:]])
-wxh = np.random.uniform(-1, 1, size=(28, 784))
-hs = np.array([range(28)])
-who = np.random.uniform(-1, 1, size=(2, 28))
-out = np.array([range(2)])
-expected = np.zeros((1, 2)) #this can be up to 5
-expected[0][data[0][0]] = 1
-alpha = 0.01
-error = 0
+#samples = random.sample(data)
 
-def predict(inputs, weights):
-    return g(np.dot(inputs, weights.T))
+def predict(inputs, weights, bias):
+    return g(np.dot(inputs, weights.T) + bias)
 
-def update(xs, wxh, hs, who, expected, out):
-    hs = predict(xs, wxh)
-    out = predict(hs, who)
+def fullPredict(inputs, hiddenWeights, outputWeights, hiddenBiases, outputBiases):
+    hiddenActivations = predict(inputs, hiddenWeights, hiddenBiases)
+    output = predict(hiddenActivations, outputWeights, outputBiases)
+    return output
+
+def decideNumber(outputActivation):
+    #outs = outputActivation.flatten() #(1, 2) to (2,)
+
+    max = 0
+    index = -1
+    for idx, point in enumerate(outputActivation[0]): #outs:
+        if point > max:
+            max = point
+            index = idx
+
+    return index
+
+#index of correct number
+def isGuessCorrect(output, index):
+    return output == index
+
+    correct = True
+    for digit, idx in enumerate(output):
+        if idx == index: #that digit should be 1
+            if digit != 1:
+                correct = False
+        else: #that digit should be 0
+            if digit != 0:
+                correct = False
+    return correct
+
+
+def update(xs, wxh, hs, who, expected, out, bh, bo):
+    hs = predict(xs, wxh, bh)
+    out = predict(hs, who, bo)
 
     error = np.sum(expected - out)
 
@@ -388,128 +356,104 @@ def update(xs, wxh, hs, who, expected, out):
     deltah = hs * (1 - hs) * np.dot(deltao, who)
 
     who = who + alpha * deltao.T * hs
+    bo = bo + alpha * deltao
     wxh = wxh + alpha * deltah.T * xs
+    bh = bh + alpha * deltah
 
-    return hs, out, wxh, who, error
+    return hs, out, wxh, who, error, bh, bo
+
+def errorWholeSet(samples, hiddenWeights, outputWeights, hiddenBiases, outputBiases):
+    correct = 0
+    incorrect = 0
+    for sample in samples:
+        input = sample[1:]
+        prediction = fullPredict(input, hiddenWeights, outputWeights, hiddenBiases, outputBiases)
+        output = decideNumber(prediction)
+        if output == sample[0]:
+            correct += 1
+        else:
+            incorrect += 1
+    return (correct), (incorrect)
+
+xs = np.array([data[0][1:]])
+wxh = np.random.uniform(-1, 1, size=(28, 784))
+hs = np.array([range(28)])
+bh = np.array([0.5 for x in hs])
+who = np.random.uniform(-1, 1, size=(2, 28))
+#who = np.random.uniform(-1, 1, size=(5, 28))
+out = np.array([range(2)])
+#out = np.array([range(5)])
+bo = np.array([0.5 for x in out])
+#expected = np.zeros((1, 2)) #this can be up to 5
+#expected = np.zeros((1, 5)) #this can be up to 5
+#expected[0][data[0][0]] = 1
+alpha = 0.01
+error = 0
+
+
+#xs = np.array([-3, 2, -1]) #test
+#wxh = np.array([[-2, 0.5], [0.5, -1.5], [0.75, 0.6]])
+#hs = np.array([])
+#who = np.array([-0.25, 0.1])
+#bh = 0
+#bh = 0
+sample = data[0]
+
+#hs, out, wxh, who, error, bh, bo = update(xs, wxh, hs, who, sample[0], predict, bh, bo)
+
+print(hs, out, wxh, who, error, bh, bo)
 
 count = 0
 error = 1
 
-xs = np.array([data[count][1:]])
+#xs = np.array([data[count][1:]])
 
-predicts = predict(xs, wxh)
-predicts = predict(hs, who)
 
-while abs(error) > 0.1:
-    xs = np.array([data[count+1][1:]])
-    hs, out, wxh, who, error = update(xs, wxh, hs, who, data[count][0], predict)
-    print(out)
-    print(error)
+
+samples = data
+epochs = 0
+incorrectRatio = 1
+while incorrectRatio > 0.05 and epochs < 20:
+    epochs += 1
+    np.random.shuffle(samples)
+    for sample in samples:
+        xs = np.array([sample[1:]])
+        out = fullPredict(xs, wxh, who, bh, bo)
+        hs, out, wxh, who, error, bh, bo = update(xs, wxh, hs, who, sample[0], out, bh, bo)
+        
+    numcorrect, numincorrect = errorWholeSet(samples, wxh, who, bh, bo)
+    incorrectRatio = numincorrect / len(samples)
+    print(numcorrect/len(samples), numincorrect/len(samples))
     print()
-    count += 1
+
+
+testData = importData("data/mnist_test_0_1.csv")
+#testData = importData("data/mnist_test_0_4.csv")
+
+print("Test error:")
+numcorrectTest, numincorrectTest = errorWholeSet(testData, wxh, who, bh, bo)
+print(numincorrectTest)
+print(numcorrectTest)
 
 
 
 
+def showDrawing(sample):
+    pixels = sample[1:]
+    pixels = [255 - x for x in pixels]
+    pixels = np.reshape(pixels, (28, 28))
+    image = PIL.Image.fromarray(pixels.astype(np.uint8))
+    image.show()
 
 
-
-#while err > .5:
-#    fullUpdate(outputNeurons, outputWeights, finalInputs, err, hiddenNeurons, hiddenWeights, hiddenInputs)
-
-
-
-"""
-outputWeights = updateWeights(outputNeurons, outputWeights, finalInputs, alpha, err, point)
-hiddenWeights = updateWeights(hiddenNeurons, hiddenWeights, hiddenInputs, alpha, err, point)
-
-hiddenInputs, hiddenNeurons, finalInputs, outputNeurons = feedForward(initX)
-
-print(outputNeurons)
-#print(outputWeights)
-err = getError(outputNeurons, data[0])
-print(err)
-print()
-
-#outputWeights = np.array(updatedOutputWeights)
-#hiddenWeights = np.array(updatedHiddenWeights)
-
-
-#outputWeights = outputWeights.reshape(28, 2)
-#hiddenWeights = hiddenWeights.reshape(785, 28)
-
-
-outputWeights = updateWeights(outputNeurons, outputWeights, finalInputs, alpha, err, point)
-hiddenWeights = updateWeights(hiddenNeurons, hiddenWeights, hiddenInputs, alpha, err, point)
-
-hiddenInputs, hiddenNeurons, finalInputs, outputNeurons = feedForward(initX)
-
-print(outputNeurons)
-#print(outputWeights)
-err = getError(outputNeurons, data[0])
-print(err)
-print()
-"""
-
-"""
-outputWeights = updateWeights(outputNeurons, outputWeights, finalInputs, alpha, err, point)
-hiddenWeights = updateWeights(hiddenNeurons, hiddenWeights, hiddenInputs, alpha, err, point)
-
-hiddenInputs, hiddenNeurons, finalInputs, outputNeurons = feedForward(initX)
-
-print(outputNeurons)
-err = getError(outputNeurons, data[0])
-print(err)
-print()
-
-
-
-
-
-outputWeights = updateWeights(outputNeurons, outputWeights, finalInputs, alpha, err, point)
-hiddenWeights = updateWeights(hiddenNeurons, hiddenWeights, hiddenInputs, alpha, err, point)
-
-hiddenInputs, hiddenNeurons, finalInputs, outputNeurons = feedForward(initX)
-
-print(outputNeurons)
-err = getError(outputNeurons, data[0])
-print(err)
-print()
-
-
-
-
-
-outputWeights = updateWeights(outputNeurons, outputWeights, finalInputs, alpha, err, point)
-hiddenWeights = updateWeights(hiddenNeurons, hiddenWeights, hiddenInputs, alpha, err, point)
-
-hiddenInputs, hiddenNeurons, finalInputs, outputNeurons = feedForward(initX)
-
-print(outputNeurons)
-err = getError(outputNeurons, data[0])
-print(err)
-print()
-
-
-
-
-
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for i in range(10):
+    prediction = fullPredict(testData[i][1:], wxh, who, bh, bo)
+    finaloutput = decideNumber(prediction)
+    print("Prediction:")
+    #for x, idx in enumerate(finaloutput):
+    #    if x == 0:
+    #        print(idx)
+    print(finaloutput)
+    showDrawing(testData[i])
+    input("hit enter for next guess:")
 
